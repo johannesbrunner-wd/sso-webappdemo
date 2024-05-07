15d8480f78debf48125be7c113065db0f6852618
Johannes Brunner <johannes.brunner@world-direct.at>
2024-05-07 16:25:08 +0200

    add connection to cosmos db.

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -1,12 +1,31 @@
-const { app } = require('@azure/functions');
+const { app, output } = require('@azure/functions');
+const sendToCosmosDb = output.cosmosDB({
+    databaseName: 'my-database',
+    containerName: 'my-container',
+    createIfNotExists: false,
+    connection: 'CosmosDBConnectionString',
+  });
 
 app.http('httpTrigger1', {
     methods: ['GET', 'POST'],
+    extraOutputs: [sendToCosmosDb],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';
+
+        try {
+            // Output to Database
+            context.extraOutputs.set(sendToCosmosDb, {
+            // create a random ID
+            id: new Date().toISOString() + Math.random().toString().substring(2, 10),
+            name: name,
+            });
+        } catch (error) {
+            context.log(`Error: ${error}`);
+            return { status: 500, body: 'Internal Server Error' };
+        }
 
         return { jsonBody: { text: `Hello, ${name}!` } };
     }
