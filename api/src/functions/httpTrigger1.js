3dfbbb0b33c4885631583166c9c17898a3df277b
Johannes Brunner <johannes.brunner@world-direct.at>
2024-05-07 17:15:37 +0200

    log statement?

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -19,6 +19,7 @@ app.http('httpTrigger1', {
         try {
             // Output to Database
             const doc = context.extraInputs.get(cosmosInput);
+            context.log(doc);
         } catch (error) {
             context.log(`Error: ${error}`);
             return { status: 500, jsonBody: { text: 'Internal Server Error' } };
