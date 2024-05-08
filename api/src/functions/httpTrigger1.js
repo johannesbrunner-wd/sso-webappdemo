9c6fcadeff58ea9323d3949244e7decb48eea614
Johannes Brunner <johannes.brunner@world-direct.at>
2024-05-08 08:42:38 +0200

    set name output from db.

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -19,6 +19,7 @@ app.http('httpTrigger1', {
             // Output to Database
             const doc = context.extraInputs.get(cosmosInput);
             context.log(doc);
+            name = doc[0].msg;
         } catch (error) {
             context.log(`Error: ${error}`);
             return { status: 500, jsonBody: { text: 'Internal Server Error' } };
